e2e503a9663521427973d8bd505852356ad237b1
AtlasLioness <narjisaliane@gmail.com>
2024-08-18 23:37:57 +0000

    4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,19 +10,23 @@ env.hosts = ['3.86.7.210', '100.25.202.154']
 
 def do_clean(number=0):
     """Fabric command to delete old archives."""
-    number = 1 if int(number) == 0 else int(number)
+    number = int(number)
+    if number < 1:
+        number = 1
 
     local_versions_dir = "versions"
     local_files = sorted(listdir(local_versions_dir))
+    files_to_keep = local_files[-number:]
     files_to_remove = local_files[:-number]
 
-    for file in files_to_remove:
-        local(f"sudo rm ./{local_versions_dir}/{file}")
+    for f in files_to_remove:
+        local("sudo rm ./{}/{}".format(local_versions_dir, f))
 
     with cd("/data/web_static/releases"):
         remote_files = run("ls -tr").split()
-        remote_files = [f for f in remote_files if "web_static_" in f]
+        remote_files = [r for r in remote_files if "web_static_" in r]
+        files_to_keep = remote_files[-number:]
         files_to_remove = remote_files[:-number]
 
-        for file in files_to_remove:
-            run(f"sudo rm -rf ./{file}")
+        for i in files_to_remove:
+            run("sudo rm -rf ./{}".format(i))
