765d61e1df23aa78dec19c5f23367a752770b44a
AtlasLioness <narjisaliane@gmail.com>
2024-08-20 00:19:52 +0000

    7. Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,4 +76,4 @@ class DBStorage:
         """remove() method on private session attribute"""
         if self.__session is not None:
             self.__session.remove()
-        self.reload()
+        self.reload() # DBStorage creates a new Session after close()
