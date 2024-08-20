cf90cd665d81c41bedda8333e7545b25c4d9b667
AtlasLioness <narjisaliane@gmail.com>
2024-08-20 20:23:55 +0000

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,14 +3,13 @@
 Module that starts a Flask web app.
 """
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def state_list():
+@app.route('/states_list', strict_slashes=False)
+def states_list():
     """Displays HTML page with states listed in a-z order"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
