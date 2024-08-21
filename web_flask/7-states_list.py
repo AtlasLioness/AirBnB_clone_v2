c60cd15141d9279ac44353dc6f41f2cf5d6b52dd
AtlasLioness <narjisaliane@gmail.com>
2024-08-21 17:49:38 +0000

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,22 +3,22 @@
 Module that starts a Flask web app.
 """
 from flask import Flask, render_template
-from models import *
 from models import storage
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """removes SQLAlchemy session after request"""
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays HTML page with states listed in a-z order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+
+    states = storage.all("State").values()
     return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """removes SQLAlchemy session after request"""
-    storage.close()
 
 
 if __name__ == "__main__":
